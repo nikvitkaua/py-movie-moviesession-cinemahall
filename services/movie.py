a830b465f29edd3b85606102bc8fdce1074809b7
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-10-26 17:48:11 +0300

    fix last err and correct flake8

--- services/movie.py
+++ services/movie.py
@@ -3,12 +3,16 @@ from django.db.models import QuerySet
 from db.models import Movie
 
 
-def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None) -> QuerySet:
+def get_movies(
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
+) -> QuerySet:
     if not genres_ids and not actors_ids:
         return Movie.objects.all()
 
     if genres_ids and actors_ids:
-        return Movie.objects.filter(genres__in=genres_ids).filter(actors__in=actors_ids)
+        return (Movie.objects.filter(genres__in=genres_ids)
+                .filter(actors__in=actors_ids))
 
     if genres_ids and not actors_ids:
         return Movie.objects.filter(genres__in=genres_ids)
@@ -16,8 +20,10 @@ def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None) -> Qu
     if actors_ids and not genres_ids:
         return Movie.objects.filter(actors__in=actors_ids)
 
+
 def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
+
 
 def create_movie(
         movie_title: str,
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timedelta
+
 from django.db.models import QuerySet
 
-from db.models import MovieSession
+from db.models import MovieSession, CinemaHall, Movie
 
 
 def create_movie_session(
@@ -8,23 +10,33 @@ def create_movie_session(
         movie_id: int,
         cinema_hall_id: int
 ) -> None:
+    movie = Movie.objects.get(id=movie_id)
+    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+
     MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=cinema_hall_id,
-        movie=movie_id
+        movie=movie,
+        cinema_hall=cinema_hall
     )
+
 
 def get_movies_sessions(session_date: str = None) -> MovieSession | QuerySet:
     """
-    Process a date in the format 'YYYY-MM-DD'.
+    Process a date in the format "YYYY-MM-DD".
     """
     if session_date:
-        return MovieSession.objects.filter(show_time=session_date)
+        start_date = datetime.strptime(session_date, "%Y-%m-%d")
+        end_date = start_date + timedelta(days=1)
+
+        return (MovieSession.objects
+                .filter(show_time__gte=start_date, show_time__lt=end_date))
 
     return MovieSession.objects.all()
 
+
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
+
 
 def update_movie_session(
         session_id: int,
@@ -34,13 +46,14 @@ def update_movie_session(
 ) -> None:
     update_fields = {}
     if show_time is not None:
-        update_fields['show_time'] = show_time
+        update_fields["show_time"] = show_time
     if movie_id is not None:
-        update_fields['movie_id'] = movie_id
+        update_fields["movie_id"] = movie_id
     if cinema_hall_id is not None:
-        update_fields['cinema_hall_id'] = cinema_hall_id
+        update_fields["cinema_hall_id"] = cinema_hall_id
     if update_fields:
         MovieSession.objects.filter(id=session_id).update(**update_fields)
+
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.filter(id=session_id).delete()
